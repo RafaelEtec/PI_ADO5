c79e70b029d8c66df0c1f3dad4bb62846e980bcf
raf <raf@DESKTOP-PN9HH5P>
2022-03-19 17:39:42 -0300

    Atividade Finalizada

--- src/main/java/Estrutura_de_Repeticao/PI_ADO5.java
+++ src/main/java/Estrutura_de_Repeticao/PI_ADO5.java
@@ -1,40 +1,51 @@
 package Estrutura_de_Repeticao;
 import java.util.Scanner;
-
+/**
+   por Rafael Ferreira Goulart
+**/
 public class PI_ADO5 {
     public static void main(String[] args) {
         Scanner ent = new Scanner(System.in);
         
-        int escolhaMenu = 0;
-        int saida = 0;
-        
-        System.out.println(
+        boolean saida = false;
+        String menu = 
                   "O que você deseja fazer?\n"
                 + "<1> Instruções\n"
                 + "<2> Jogar\n"
                 + "<3> Créditos\n"
-                + "<4> Sair \n");
-        escolhaMenu = ent.nextInt();
+                + "<4> Sair \n";
+        
+        System.out.println(menu);
+        int escolhaMenu = ent.nextInt();
         
         do {
             switch (escolhaMenu) {
                 case 1:
-                    System.out.println("Você escolheu entrar nas Instruções.");
-                    saida = 1;
+                    System.out.println("Você escolheu as Instruções.\n");
+                    System.out.println(menu);
+                    escolhaMenu = ent.nextInt();
                     break;
                 case 2:
-                    System.out.println("Você escolheu entrar nas Jogar.");
-                    saida = 2;
+                    System.out.println("Você escolheu Jogar.\n");
+                    System.out.println(menu);
+                    escolhaMenu = ent.nextInt();
                     break;
                 case 3:
-                    System.out.println("Você escolheu entrar nas Créditos.");
-                    saida = 3;
+                    System.out.println("Você escolheu os Créditos.\n");
+                    System.out.println(menu);
+                    escolhaMenu = ent.nextInt();
                     break;
                 case 4:
-                    System.out.println("Você escolheu Sair do Programa.");
-                    saida = 4;
+                    System.out.println("Você escolheu Sair do Programa.\n");
+                    saida = true;
+                    break;
+                default:
+                    System.out.println(
+                          "Você não escolheu nada ou inseriu um número inutilizável!\n"
+                        + "Favor insira novamente o número: ");
+                    escolhaMenu = ent.nextInt();
                     break;
             }
-        } while (escolhaMenu != 4);
+        } while (!saida);
     }
 }
