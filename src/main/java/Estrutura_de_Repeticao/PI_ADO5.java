adef621db5d730e3ab12db2dd1f4168f2fbd7c41
vinicius.soliveira63 <vinicius.soliveira63@CAS511911C138.senacsp.edu.br>
2022-03-18 21:30:20 -0300

    18/03/2022 - Finalizando Atividade

--- src/main/java/Estrutura_de_Repeticao/PI_ADO5.java
+++ src/main/java/Estrutura_de_Repeticao/PI_ADO5.java
@@ -5,9 +5,36 @@ public class PI_ADO5 {
     public static void main(String[] args) {
         Scanner ent = new Scanner(System.in);
         
+        int escolhaMenu = 0;
+        int saida = 0;
+        
+        System.out.println(
+                  "O que você deseja fazer?\n"
+                + "<1> Instruções\n"
+                + "<2> Jogar\n"
+                + "<3> Créditos\n"
+                + "<4> Sair \n");
+        escolhaMenu = ent.nextInt();
         
         do {
-            
-        } while (true);
+            switch (escolhaMenu) {
+                case 1:
+                    System.out.println("Você escolheu entrar nas Instruções.");
+                    saida = 1;
+                    break;
+                case 2:
+                    System.out.println("Você escolheu entrar nas Jogar.");
+                    saida = 2;
+                    break;
+                case 3:
+                    System.out.println("Você escolheu entrar nas Créditos.");
+                    saida = 3;
+                    break;
+                case 4:
+                    System.out.println("Você escolheu Sair do Programa.");
+                    saida = 4;
+                    break;
+            }
+        } while (escolhaMenu != 4);
     }
 }
